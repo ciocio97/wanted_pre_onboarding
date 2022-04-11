013efe2b7f089b0e33385ea51501772d7f6286ad
ciocio97 <tmddusleo@icloud.com>
2022-04-11 23:56:24 +0900

    create : entire screen structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+// react
+import React from 'react';
+// style
 import './App.css';
+import styled from 'styled-components';
+// components
+import Toggle from './components/Toggle';
+
+const Container = styled.div`
+  height: auto;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`
+const RenderContainer = styled.div `
+  position: relative;
+  width: 90%;
+  height: 400px;
+  margin: 20px 0px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid rgba(105, 105, 105, 0.2);
+`
+const Title = styled.p `
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  margin: 0;
+  font-size: 1.5em;
+  font-weight: 700;
+`
 
 function App() {
+  const components = [
+    { name: 'Toggle', element: <Toggle /> , },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+    {components.map((item, idx) => ( 
+       <Container key = { idx }>
+         <RenderContainer>
+         <Title> { item.name } </Title> 
+         { item.element } 
+         </RenderContainer> 
+       </Container>    
+     ))} 
+    </>
   );
 }
 
